66b20b7333b3806ee3c92efd4867d3441d437ec0
Hartej Singh <hrtjsingh@gmail.com>
2025-08-22 21:12:47 +0530

    issue fixed

--- models/Playlist.ts
+++ models/Playlist.ts
@@ -1,7 +1,7 @@
 import mongoose, { Schema, model, models } from "mongoose";
 
 const songSchema = new Schema({
-  id: { type: String, required: true, unique: true },
+  id: { type: String, required: true },
 });
 
 const PlaylistSchema = new Schema(
